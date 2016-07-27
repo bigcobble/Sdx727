02b383a1430c7a02593559de4a47e7a08c6a8ebd
bigcobble1 <260378950@qq.com>
2016-07-27 16:36:26 +0800

    my third

--- app/src/main/java/com/example/lsx/sdx727/MainActivity.java
+++ app/src/main/java/com/example/lsx/sdx727/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //ssssss
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
