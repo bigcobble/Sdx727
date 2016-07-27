98ae55a34f1c8027f908e61a75a6dfd4c3c3db3d
bigcobble1 <260378950@qq.com>
2016-07-27 16:25:17 +0800

    my second commit

---
+++ app/src/main/java/com/example/lsx/sdx727/welcome/WelcomeActivity.java
@@ -0,0 +1,11 @@
+package com.example.lsx.sdx727.welcome;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/27.
+ */
+public class WelcomeActivity extends AppCompatActivity{
+
+}
+
